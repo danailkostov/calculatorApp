699fc1a478235960b159e00765cf4fff0bfc7504
danailkostov <dankostov359@gmail.com>
2020-06-14 14:11:01 +0300

    added functionality to all buttons

--- app.js
+++ app.js
@@ -5,60 +5,111 @@ const deleteBtn = document.querySelector('.delete');
 const clearBtn = document.querySelector('.clear');
 const previousOp = document.querySelector('.previous-operand');
 const currentOp = document.querySelector('.current-operand');
-let result = 0;
-let sign;
-let firstNum;
-let secondNum;
-let arrOfNums;
-let targetSecondNum;
-let targetFirstNum;
+let sign = false;
+let signCount = 0;
+let lastSign;
+let currentResult = 0;
+let reg = /\d+\.*\d*/g;
+let numbers;
 
 //what happen when number is clicked
 numberBtns.forEach(function(btn) {
     btn.addEventListener('click', function(){
-        //what happens when in the output have at least one math.operator
-        if(sign) {;
-            currentOp.textContent += btn.textContent;
-            arrOfNums = currentOp.textContent.split(sign);
-            targetFirstNum = arrOfNums.length - 2;
-            targetSecondNum = arrOfNums.length - 1;
-            firstNum = parseInt(arrOfNums[targetFirstNum]);
-            secondNum = parseInt(arrOfNums[targetSecondNum]);
-            
-            switch(sign) {
-                case 'x':
-                    result = firstNum * secondNum;
-                    break;
-                case '/':
-                    result = firstNum / secondNum;
-                    break;
-                case '-':
-                    result = firstNum - secondNum;
-                    break;
-                case '+':
-                    result = firstNum + secondNum;
-                    break;
-            }
-            previousOp.textContent = result;
-        } else {
-            //what happens when there are only digits in output
-            currentOp.textContent += btn.textContent;
-        }
-        
+        currentOp.textContent += btn.textContent;
+        sign = false;
     })
 })
-    
 //what happens when operator sign is clicked
 operatorBtns.forEach(function(btn)  {
     btn.addEventListener('click', function(){
-       currentOp.textContent += btn.textContent; 
-       sign = btn.textContent;   
+    //extract numbers
+        numbers = currentOp.textContent.match(reg);
+        console.log(numbers);
+        for(let i = 0; i < numbers.length; i++) {
+            numbers[i] = parseFloat(numbers[i]);
+        }
+        console.log(numbers)
+    ///////////////
+    if(!sign) {
+        currentOp.textContent += btn.textContent;
+        sign = true;
+        signCount++;
+    }
+    
+    if(signCount > 1 && signCount < 3) {
+        switch(lastSign) {
+            case 'x':
+                currentResult = numbers[0] * numbers[1];
+                previousOp.textContent = currentResult;
+                console.log(currentResult);
+                break;
+            case '/':
+                currentResult = numbers[0] / numbers[1];
+                previousOp.textContent = currentResult;
+                console.log(currentResult);
+                break;
+            case '-':
+                currentResult = numbers[0] - numbers[1];
+                previousOp.textContent = currentResult;
+                console.log(currentResult);
+                break;
+            case '+':
+                currentResult = numbers[0] + numbers[1];
+                previousOp.textContent = currentResult;
+                console.log(currentResult);
+                break;
+        }
+    } else if (signCount > 2) {
+        switch(lastSign) {
+            case 'x':
+                currentResult = currentResult * numbers[numbers.length - 1];
+                previousOp.textContent = currentResult;
+                console.log(currentResult);
+                break;
+            case '/':
+                currentResult = currentResult / numbers[numbers.length - 1];
+                previousOp.textContent = currentResult;
+                console.log(currentResult);
+                break;
+            case '-':
+                currentResult = currentResult - numbers[numbers.length - 1];
+                previousOp.textContent = currentResult;
+                console.log(currentResult);
+                break;
+            case '+':
+                currentResult = currentResult + numbers[numbers.length - 1];
+                previousOp.textContent = currentResult;
+                console.log(currentResult);
+                break;
+        }
+    }           
+    // last sign
+    lastSign = btn.textContent;
     })
 })
-
+////////////////////////
 clearBtn.addEventListener('click', function() {
         previousOp.textContent = '';
         currentOp.textContent = '';
+        currentResult = 0;
+        signCount = 0;
+        
+})
+
+deleteBtn.addEventListener('click', function() {
+    if(!sign) {
+        currentOp.textContent = currentOp.textContent
+                                        .split('')
+                                        .slice(0, currentOp.textContent.length - 1)
+                                        .join('');
+    }
+    
+})
+
+equalsBtn.addEventListener('click', function() {
+        previousOp.textContent = '';
+        currentOp.textContent = currentResult;
 })
 
 
+//need to add decimals
